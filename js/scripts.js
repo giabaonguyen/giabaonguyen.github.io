0a3dd4c5ed59abdecb65976cbfe397645dd299af
Nguyễn gia bảo <giabao28996@gmail.com>
2024-07-21 22:17:27 +0700

    update code

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzwgZKIEAkCw82Z4v0lbFWcrSHUZevzU2QkXApL0YDOLXfMrlIs_KxWeaVpYrF6UB6o/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbwZ0vN4ELg2JzjRrd0F_F9x6dFHu8UqNw_I7DLm96BzZSuN5zGjKc2TQPdGn4HjnWyv/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
